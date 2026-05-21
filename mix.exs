3a5176cd50fc480cf0537e3493e5cdca719a988a
Sasha Gerrand <sgerrand@users.noreply.github.com>
2026-05-21 15:25:16 +0100

    chore(build): support Elixir >= v1.17

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule GitHoox.MixProject do
     [
       app: :git_hoox,
       version: @version,
-      elixir: "~> 1.19",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
